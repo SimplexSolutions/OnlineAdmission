39caf79c8a9575f5b31a6b21368466716e634df2
golam habib palash <golamhabibpalash@gmail.com>
2021-12-11 18:25:11 +0600

    PaymentTypesController added

---
+++ OnlineAdmission.APP/Controllers/PaymentTypesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineAdmission.BLL.IManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineAdmission.APP.Controllers
+{
+    public class PaymentTypesController : Controller
+    {
+        private readonly IPaymentTypeManager _paymentTypeManager;
+
+        public PaymentTypesController(IPaymentTypeManager paymentTypeManager)
+        {
+            _paymentTypeManager = paymentTypeManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PaymentTypesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PaymentTypesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PaymentTypesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentTypesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentTypesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentTypesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentTypesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
