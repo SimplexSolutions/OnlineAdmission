2443cd999524ad370e0f399c884f9c2ee23e195f
golam habib palash <golamhabibpalash@gmail.com>
2021-12-11 18:23:17 +0600

    MeritTypesController Added

---
+++ OnlineAdmission.APP/Controllers/MeritTypesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineAdmission.BLL.IManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineAdmission.APP.Controllers
+{
+    public class MeritTypesController : Controller
+    {
+        private readonly IMeritTypeManager _meritTypeManager;
+
+        public MeritTypesController(IMeritTypeManager meritTypeManager)
+        {
+            _meritTypeManager = meritTypeManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MeritTypesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MeritTypesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MeritTypesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MeritTypesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MeritTypesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MeritTypesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MeritTypesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
